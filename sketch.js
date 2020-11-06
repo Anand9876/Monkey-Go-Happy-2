32990058e57e79a74a1dd4147279f206aa074677
Anand9876 <73025487+Anand9876@users.noreply.github.com>
2020-11-06 19:55:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var score;
 function preload(){
   
   jungleImage=loadImage("sprite_0.png");
-  backgroundImage=loadImage("sprite_0.png");
+  backgroundImage=loadImage("sprite_1.png");
   monkey_running =
   loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png ")
   
