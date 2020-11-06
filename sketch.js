ce9380b86e8d5a4fca458f7c6aa812b3ed31a695
Anand9876 <73025487+Anand9876@users.noreply.github.com>
2020-11-06 18:48:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup() {
   jungle= createSprite(200,200,300,10);
   jungle.velocityX=-2;
   jungle.addImage("background",jungleImage);
- jungle.scale=2;
+ jungle.scale=windowWidth;
   
   ground = createSprite(width/2,height,width,2);
   ground.velocityX=-2;
